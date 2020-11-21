594ec0b387a66db6a4c3909a7d5c2b4f0d860fa9
Shaun <shaunbw4@gmail.com>
2020-11-21 21:21:32 +0200

    Updates

---
+++ precache-manifest.340cb164a8ddec4f4eab329cc928e445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac31fce0fabfbfe76096b5e5e670290",
+    "url": "/markdown/index.html"
+  },
+  {
+    "revision": "6cf2154b18fcc8d6dbe3",
+    "url": "/markdown/static/css/2.0b8fbf56.chunk.css"
+  },
+  {
+    "revision": "aea5fa67dfe0fa706b26",
+    "url": "/markdown/static/css/main.0be671b4.chunk.css"
+  },
+  {
+    "revision": "6cf2154b18fcc8d6dbe3",
+    "url": "/markdown/static/js/2.c8a05684.chunk.js"
+  },
+  {
+    "revision": "aea5fa67dfe0fa706b26",
+    "url": "/markdown/static/js/main.3e99a590.chunk.js"
+  },
+  {
+    "revision": "ac97faad974e85df079a",
+    "url": "/markdown/static/js/runtime-main.ebc499e0.js"
+  }
+]);
